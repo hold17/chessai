7c4eceef74899d42b5c15e1f882274380ebbcc60
awo-dev <anders@wiberg.tech>
2018-04-23 19:23:10 +0200

    Fix xboard implementation and a bit of cleanup

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,15 +4,20 @@ import util.Square;
 import board.Algorithm;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws FileNotFoundException {
+
+    public static void main(String[] args) throws Exception {
+        final String fileName = System.getProperty("user.dir") + "\\log.txt";
+        bum(fileName);
+    }
+
+    private static void bum(final String logPath) throws Exception {
         Scanner sc = new Scanner(System.in).useDelimiter("\\s+"); // Move skal bruge parametre, vi splitter derfor på mellemrum
         Board board = new Board();
-        File logfile = new File("C:\\Users\\Justin\\IdeaProjects\\chessai-java\\log.txt");
+        File logfile = new File(logPath);
         PrintWriter writer = new PrintWriter(logfile);
         /**
          *  Til winboard
@@ -28,41 +33,69 @@ public class Main {
                     System.exit(0);
                     break;
                 case "new":
-                    System.out.println("debug 1");
-                    System.out.println("usermove 1");
                     board.initializeBoard();
                     //Nulstil bræt
                     break;
                 case "black":
                     board.setMachineColor(Color.BLACK);
                     break;
+                case "protover":
+                    sc.next();
+                    System.out.println("feature ping=1 done=1");
+                    break;
+                case "ping":
+                    System.out.println("pong " + sc.next());
+                    break;
                 case "white":
                     board.setMachineColor(Color.WHITE);
                     break;
                 case "usermove":
                 case "move":
-                    command = sc.next();
-                    writer.write(" "+command);
+//                    command = sc.next();
+//                    writer.write(" "+command);
+                    final String moveStr = sc.next();
+                    writer.write(moveStr);
+                    move(moveStr, board);
                     // Dekod skaknotation for træk til felt indeks
-                    char[] chars = command.toCharArray();
-                    int pieceIndex = chars[0]-'a' + (chars[1]-'1')<<4; //Start index
-                    int destinationIndex = chars[2]-'a' + (chars[3]-'1')<<4; // Slut index
-                    System.out.println("#"+pieceIndex+" " + destinationIndex);
-                    board.move(pieceIndex, destinationIndex);
+
                     break;
                 case "go":
-                    Algorithm algorithm = new Algorithm();
-                    algorithm.aiPlay(board,5);
-                    System.out.println(board.getLastMove());
+                    go(board);
                     break;
+                case "accepted": case "xboard": case "random": case "level": case "post": case "hard": case "time": case "otim": break;
                 default:
-                    System.out.println("#Ukendt kommando: " + command);
+//                    System.out.println("#Ukendt kommando: " + command);
+                    if (isAMove(command)) {
+                        move(command, board);
+                    }
             }
 
             writer.println();
             sc.nextLine(); // Smid resten af linjen væk
         }
+    }
 
+    private static void move(String moveStr, Board board) {
+        char[] chars = moveStr.toCharArray();
+        int pieceIndex = Square.getSquare(moveStr.substring(0,2)).getValue();
+        int destinationIndex = Square.getSquare(moveStr.substring(2,4)).getValue();
+//        System.out.println("from: " + pieceIndex + "  to: " + destinationIndex);
+        board.move(pieceIndex, destinationIndex);
+
+        go(board);
+    }
+
+    private static void go(Board board) {
+        Algorithm algorithm = new Algorithm();
+        algorithm.aiPlay(board,5);
+        System.out.println("move" + board.getLastMove());
+    }
+
+
+    private static boolean isAMove(String message) {
+        final String regex = "[a-h][1-8][a-h][1-8]";
+
+        return message.matches(regex);
     }
 }
 
