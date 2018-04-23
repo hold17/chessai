47b51723c47c3cbdc31ddd3f61605bc6e237b316
awo-dev <anders@wiberg.tech>
2018-04-23 19:21:28 +0200

    Cleanup

--- src/main/java/board/Algorithm.java
+++ src/main/java/board/Algorithm.java
@@ -1,96 +1,108 @@
 package board;
 
 import util.Color;
+import util.FieldState;
 import util.Square;
-
 import java.util.List;
 
 public class Algorithm {
+    public Algorithm() {}
 
-    public void aiPlay(Board board, int maxply) {
-        int alpha = -100;
-        int beta = 100; // Bruges som uendelig
-        alphaBetaPruning(board,0,alpha,beta,0, maxply);
-
+    public void aiPlay(Board board, int maxPly) {
+        int alpha = Integer.MIN_VALUE;
+        int beta = Integer.MAX_VALUE; // Bruges som uendelig
+        alphaBetaPruning(board,0,alpha,beta,0, maxPly);
     }
 
-    private int alphaBetaPruning(Board board, int score, int alpha, int beta, int ply, int maxply) {
-        if(ply++ == maxply || board.gameOver) {
+    private int alphaBetaPruning(Board board, int score, int alpha, int beta, int ply, int maxPly) {
+        if(ply++ == maxPly || board.gameOver) {
             return score; // fortegn???
         }
+
         if(board.player == Color.WHITE) {
-            return getMax(board, alpha, beta, ply, maxply);
-        }else {
-            return getMin(board, alpha, beta, ply, maxply);
+            return getMax(board, alpha, beta, ply, maxPly);
+        } else {
+            return getMin(board, alpha, beta, ply, maxPly);
         }
     }
 
-    private int getMax(Board board, int alpha, int beta, int ply, int maxply) {
+    private int getMax(Board board, int alpha, int beta, int ply, int maxPly) {
         int indexOfBestPiece = -1;
         int indexOfBestMove = -1;
+
         for (int i = 0; i < board.field.length; i++) {
-            boolean validSquare = (i & 0x88) == 0;
-            if (validSquare && (board.getMachineColor() == board.field[i].getColor())){
+            if (Square.isValid(i) && (board.getMachineColor() == board.field[i].getColor())){
 //                Løb igennem samtlige træk og find det bedste
                 Rules rules = new Rules();
                 Square startSquare = Square.getSquare(i);
+
+                if (board.getFieldState(startSquare) == FieldState.EMPTY) continue;
+
                 List<Move> moveList = rules.getMoves(board, startSquare);
+
                 for (int j = 0; j < moveList.size(); j++) {
                     Square endSquare = moveList.get(j).getEndSquare();
                     int score = moveList.get(j).getScore();
                     Board modifiedBoard = board.getDeepCopy();
+
                     modifiedBoard.move(startSquare.getValue(), endSquare.getValue());
-                    score = alphaBetaPruning(modifiedBoard, score, alpha, beta, ply,maxply);
+                    score = alphaBetaPruning(modifiedBoard, score, alpha, beta, ply,maxPly);
+
                     if (score > alpha) {
                         alpha = score;
                         indexOfBestPiece = startSquare.getValue();
                         indexOfBestMove = endSquare.getValue();
-                    }                // Pruning.
-                    if (alpha >= beta) {
-                        break;
                     }
+
+                    if (alpha >= beta) break;
                 }
             }
         }
         if (indexOfBestMove != -1) {
             board.move(indexOfBestPiece,indexOfBestMove);
         }
+
         return alpha;
     }
     // Fra gamle Kryds og Bolle
-    private int getMin(Board board, int alpha, int beta, int ply, int maxply) {
+    private int getMin(Board board, int alpha, int beta, int ply, int maxPly) {
         int indexOfBestPiece = -1;
         int indexOfBestMove = -1;
+
         for (int i = 0; i < board.field.length; i++) {
-            boolean validSquare = (i & 0x88) == 0;
-            if (validSquare && (board.getMachineColor() == board.field[i].getColor())){
+            if (Square.isValid(i) && (board.getMachineColor() == board.field[i].getColor())){
 //                Løb igennem samtlige træk og find det bedste
                 Rules rules = new Rules();
                 Square startSquare = Square.getSquare(i);
+
+                if (board.getFieldState(startSquare) == FieldState.EMPTY) continue;
+
                 List<Move> moveList = rules.getMoves(board, startSquare);
+
                 for (int j = 0; j < moveList.size(); j++) {
                     Square endSquare = moveList.get(j).getEndSquare();
                     int score = -moveList.get(j).getScore();
                     Board modifiedBoard = board.getDeepCopy();
+
                     modifiedBoard.move(startSquare.getValue(), endSquare.getValue());
-                    score = alphaBetaPruning(modifiedBoard, score, alpha, beta, ply,maxply);
+                    score = alphaBetaPruning(modifiedBoard, score, alpha, beta, ply,maxPly);
+
                     if (score < beta) {
                         beta = score;
                         indexOfBestPiece = startSquare.getValue();
                         indexOfBestMove = endSquare.getValue();
-                    }                // Pruning.
-                    if (alpha >= beta) {
-                        break;
                     }
+                    // Pruning.
+                    if (alpha >= beta) break;
                 }
             }
         }
         if (indexOfBestMove != -1) {
             board.move(indexOfBestPiece,indexOfBestMove);
         }
+
         return beta;
     }
-
 
 
 
